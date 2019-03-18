b5bc44b5df05eedb5358c57adaab99e4a48a1465
cuisongliu <cuisongliu@qq.com>
2019-03-18 11:50:19 +0800

    fix drone

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+git clone https://github.com/kakawait/hugo-tranquilpeak-theme.git themes/hugo-tranquilpeak-theme
+hugo   -t hugo-tranquilpeak-theme
+
+git clone git@github.com:cuisongliu/cuisongliu.github.io.git
+cd cuisongliu.github.io && rm -rf *
+cp ../public/* .
+git add .
+git commit -am "shell commit"
+git push   origin master
+cd ../
+rm -rf cuisongliu.github.io
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -x
+git clone https://github.com/kakawait/hugo-tranquilpeak-theme.git themes/hugo-tranquilpeak-theme
+hugo server  -t hugo-tranquilpeak-theme --watch
